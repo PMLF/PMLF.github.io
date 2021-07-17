05e8be412c048d8f315a706e143c23be875a372e
Pedro Fonseca <pmlfonseca@outlook.com>
2021-07-17 20:12:53 +0100

    Added media queries to Name and Face.

--- pmlf/src/containers/Landing/index.js
+++ pmlf/src/containers/Landing/index.js
@@ -13,6 +13,9 @@ const SectionStyled = styled('section')`
     flex-direction: column;
     position: relative;
 
+    @media only screen and (max-width: ${darkTheme.breakpoint.md}) {
+        height: 700px;
+    }
     h1,
     h2 {
         text-transform: uppercase;
@@ -21,16 +24,6 @@ const SectionStyled = styled('section')`
         font-weight: 400;
         text-align: center;
         margin: 0;
-    }
-    h1 {
-        font-size: 70px;
-    }
-    .logo {
-        height: 70%;
-        margin: 20px auto 0;
-        position: relative;
-        z-index: 1;
-        max-width: 50%;
     }
     .footballFan {
         height: 400px;
@@ -54,13 +47,33 @@ const SectionStyled = styled('section')`
     }
 `
 
+const Name = styled('h1')`
+    font-size: 70px;
+    @media only screen and (max-width: ${darkTheme.breakpoint.md}) {
+        font-size: 58px;
+    }
+`
+
+const Face = styled('img')`
+    max-height: 70%;
+    margin: 20px auto 0;
+    position: relative;
+    z-index: 1;
+    max-width: 75%;
+    
+    @media only screen and (max-width: ${darkTheme.breakpoint.lg}) {
+        margin: 40px auto 0;
+        max-width: 70%;
+    }
+`
+
 export default function Landing() {
 
     return (
         <SectionStyled>
-            <h1>Pedro Fonseca</h1>
+            <Name>Pedro Fonseca</Name>
             <h2>Front End dev | Football fan</h2>
-            <img src={Logo} alt="Logo" className="logo" />
+            <Face src={Logo} alt="Logo" className="logo" />
             <div className="footballFan">
                 <img src={FootballFanBorders} alt="Football fan"/>
             </div>
