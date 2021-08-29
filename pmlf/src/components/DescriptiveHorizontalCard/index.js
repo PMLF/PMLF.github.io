21d97e6243474070050e95bc069ac53abc411623
Pedro Fonseca <pmlfonseca@outlook.com>
2021-08-29 17:30:36 +0100

    Changed the DHCard display styles.

--- pmlf/src/components/DescriptiveHorizontalCard/index.js
+++ pmlf/src/components/DescriptiveHorizontalCard/index.js
@@ -3,32 +3,41 @@ import styled from "styled-components";
 import { darkTheme } from "../../themes";
 
 const Card = styled("div")`
-	width: 100%;
+	width: calc(100% - 80px);
 	background-color: white;
 	margin: 40px 0;
 	padding: 40px;
 	border-radius: 20px;
 	display: flex;
-	align-items: flex-start;
+	align-items: center;
+	flex-direction: column;
+`;
+
+const CardTop = styled("div")`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: flex-start;
+	width: 100%;
 `;
 
 const Image = styled("img")`
-	height: 160px;
-`;
-
-const TextDiv = styled("div")`
-	font-family: ${darkTheme.fontFamily.main};
-	margin-left: 40px;
+	height: 80px;
+	margin-right: 20px;
 `;
 
 const CardTitle = styled("p")`
 	font-size: 26px;
-	margin-bottom: 10px;
+	margin: 10px 0;
 `;
 
 const CardSubtitle = styled("p")`
 	font-size: 20px;
 	margin: 10px 0;
+`;
+
+const TextDiv = styled("div")`
+	font-family: ${darkTheme.fontFamily.main};
 `;
 
 export default function DescriptiveHorizontalCard({
@@ -40,12 +49,14 @@ export default function DescriptiveHorizontalCard({
 }) {
 	return (
 		<Card>
-			<Image src={imgSrc} alt={imgAlt} />
-			<TextDiv>
-				<CardTitle>{title}</CardTitle>
-				<CardSubtitle>{subtitle}</CardSubtitle>
-				{children}
-			</TextDiv>
+			<CardTop>
+				<Image src={imgSrc} alt={imgAlt} />
+				<div>
+					<CardTitle>{title}</CardTitle>
+					<CardSubtitle>{subtitle}</CardSubtitle>
+				</div>
+			</CardTop>
+			<TextDiv>{children}</TextDiv>
 		</Card>
 	);
 }
